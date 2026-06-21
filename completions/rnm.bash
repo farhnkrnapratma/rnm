fe49ed1930dc5202d2eaf9e06feaedfb53cf4b32
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2026-06-21 20:21:09 +0700

    fix(install): update is_group_or_other_writable to allow group-owned path

--- completions/rnm.bash
+++ completions/rnm.bash
@@ -1,29 +1,30 @@
 # bash completion for rnm
-_rnm() {
-	local cur prev opts completion_modes
-	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD - 1]}"
-	opts="--dry-run -d --disable-log --rollback --camel-case --pascal-case --snake-case --screaming-snake-case --kebab-case --install-completions --gen-config -c --install -i --reinstall -r --update -u --uninstall -x --purge -p --help -h --"
-	completion_modes="bash fish zsh nu all detect"
+_rnm()
+{
+  local cur prev opts completion_modes
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD - 1]}"
+  opts="--dry-run -d --disable-log --rollback --camel-case --pascal-case --snake-case --screaming-snake-case --kebab-case --install-completions --gen-config -c --install -i --reinstall -r --update -u --uninstall -x --purge -p --help -h --"
+  completion_modes="bash fish zsh nu all detect"
 
-	if [[ $prev == --install-completions ]]; then
-		mapfile -t COMPREPLY < <(compgen -W "$completion_modes" -- "$cur")
-		return 0
-	fi
+  if [[ $prev == --install-completions ]]; then
+    mapfile -t COMPREPLY < <(compgen -W "$completion_modes" -- "$cur")
+    return 0
+  fi
 
-	if [[ $cur == --install-completions=* ]]; then
-		mapfile -t COMPREPLY < <(compgen -W "$completion_modes" -- "${cur#*=}")
-		COMPREPLY=("${COMPREPLY[@]/#/--install-completions=}")
-		return 0
-	fi
+  if [[ $cur == --install-completions=* ]]; then
+    mapfile -t COMPREPLY < <(compgen -W "$completion_modes" -- "${cur#*=}")
+    COMPREPLY=("${COMPREPLY[@]/#/--install-completions=}")
+    return 0
+  fi
 
-	if [[ $cur == -* ]]; then
-		mapfile -t COMPREPLY < <(compgen -W "$opts" -- "$cur")
-		return 0
-	fi
+  if [[ $cur == -* ]]; then
+    mapfile -t COMPREPLY < <(compgen -W "$opts" -- "$cur")
+    return 0
+  fi
 
-	mapfile -t COMPREPLY < <(compgen -d -- "$cur")
+  mapfile -t COMPREPLY < <(compgen -d -- "$cur")
 }
 
 complete -F _rnm rnm
